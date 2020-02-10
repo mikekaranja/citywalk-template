66f2573008efea93c745a723e0b45d73d7908299
wekesa931 <wekesabill@gmail.com>
2020-02-10 17:11:12 +0300

    purge unused code

--- components/widgets/tap-to-top.vue
+++ components/widgets/tap-to-top.vue
@@ -1,10 +1,12 @@
 <template>
-    <div>
-        <go-top class="tap-top top-cls" :size=50 bg-color="#222222"></go-top>
-    </div>
+  <!-- eslint-disable -->
+  <div>
+    <go-top class="tap-top top-cls" :size=50 bg-color="#222222"></go-top>
+  </div>
 </template>
 
 <script>
+/* eslint-disable */
 import GoTop from '@inotom/vue-go-top'
 export default {
   components: {
